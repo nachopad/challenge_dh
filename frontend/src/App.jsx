9385eb294fab2de67a49ea2dad8bcca6569388da
MaxiCarrillo <maxi11carrillo@gmail.com>
2024-07-07 17:51:34 +0900

    Add comment about routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,26 @@ const routes = [
   },
   {
     path: '/example',
-    component: <div style={{padding: "1rem", background: "#f09"}}>Ejemplo</div>,
+    component: <div style={{ padding: "1rem", background: "#f09" }}>Ejemplo</div>,
     exact: true
   }
+
+  /*
+  TODO: Eliminar este comentario y completar la lista de rutas
+
+  El resultado final deberia quedar algo asi:
+  
+  {
+      path: '/',
+      component: Home,
+      exact: true
+  }
+
+  Donde Home es un componente que se importa de un archivo separado
+
+  import Home from './Home.jsx'
+  */
+ 
 ]
 
 function App() {
