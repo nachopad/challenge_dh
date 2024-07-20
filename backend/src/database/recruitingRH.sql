33b2972d59fb5138a38f97cb699cb1702a715fa5
Ignacio A. Padilla <nacho20002008@gmail.com>
2024-07-20 12:02:50 -0300

    adding cascading removal for applicants and professions

--- backend/src/database/recruitingRH.sql
+++ backend/src/database/recruitingRH.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE recruitingRH_db;
 
 CREATE TABLE recruitingRH_db.Applicants(
-	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     dni VARCHAR(8) NOT NULL,
     first_name VARCHAR(15) NOT NULL,
     last_name VARCHAR(25) NOT NULL,
@@ -14,15 +14,15 @@ CREATE TABLE recruitingRH_db.Applicants(
 );
 
 CREATE TABLE recruitingRH_db.Professions(
-	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(50),
     description VARCHAR(2000)
 );
 
 CREATE TABLE recruitingRH_db.Applicant_Professions(
-	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     applicant_id INT UNSIGNED,
     profession_id INT UNSIGNED,
-    FOREIGN KEY(applicant_id) REFERENCES Applicants(id),
+    FOREIGN KEY(applicant_id) REFERENCES Applicants(id) ON DELETE CASCADE,
     FOREIGN KEY(profession_id) REFERENCES Professions(id)
 );
